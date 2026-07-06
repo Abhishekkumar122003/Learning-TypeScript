a9caf24b19082eb508c54017c12b6d697a26f697
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-06 16:34:40 +0530

    i learned how to create the variable of string , number,array, also what is type "any"doo

--- typeScript/index.ts
+++ typeScript/index.ts
@@ -1,1 +1,25 @@
 console.log("Hello via Bun!");
+function greet(name:any){
+    console.log(name)
+}
+
+let x =1; // here you don't tell what type is it but still the typescript know by looking that "x & y" looks like number
+let y =2;  //this is called inference in typescrit
+
+console.log(x + y)
+
+// Write a function first_element
+// that takes a array as an input,
+// returns the first element if it exists,
+// if it doesn;t exists, then return null
+let a :number[] =[4,5,6,1];
+
+function first_element(arr:number[]){
+    if(arr.length>0){
+        return arr[0];
+    }else{
+        return null;
+    }
+}
+
+console.log(first_element(a));
