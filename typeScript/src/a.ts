a7233ca821568f0b712b28adb122b06c004a72c8
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-22 20:27:00 +0530

    i use enum to declear the types of data function can expect

--- typeScript/src/a.ts
+++ typeScript/src/a.ts
@@ -1,14 +1,22 @@
 type keyInput = "up" | "down" | "left" | "right" ; // type is a way to provide the type->data which can be passed to the function  
 
-//now i can also use "ENUS" for
+//now i can also use "ENUS" for determining the value that are use to declear the type of the argument
+
+enum Direction { // enum => i can iterate through the value inside it
+    Up,
+    Down,
+    Left,
+    Right
+}
 
 
-function doSomething(keyPressed: keyInput ){
+function doSomething(keyPressed: /**keyInput*/ Direction ){
     //do something
-    if(keyPressed =="up"){
+    if(keyPressed == Direction.Down){
 
     }
     // some more operation
 }
 
-doSomething("up");
+doSomething(Direction.Down);
+doSomething(Direction.Left);
