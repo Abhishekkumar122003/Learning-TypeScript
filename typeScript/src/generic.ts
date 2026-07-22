eef22bc7843bc6485ee8c12e63774ca5912c6b9e
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-07-22 22:39:37 +0530

    .

--- typeScript/src/generic.ts
+++ typeScript/src/generic.ts
@@ -5,6 +5,16 @@ function firstEl(arr: Input[]){
     return arr[0];
 
 }
-
+function secondEl(arr:String [] | Number[]){
+    return arr[0]
+}
 const value = firstEl(["hrkirt" , "sigh"]);
-console.log(value)
+const value1 = secondEl(["hrkirt" , "sigh"  ]);
+// console.log(value1);
+
+function identity<T>(arg:T[]):T{
+    return arg[0];
+}
+
+const output1 = identity<string>(["string"]);
+console.log(output1.toUpperCase())
